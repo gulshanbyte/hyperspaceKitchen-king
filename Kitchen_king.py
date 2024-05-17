c8139fb9e1b3426f023d24365de591ea66579739
gulshan <gulshanmahour@gmail.com>
2024-05-17 14:53:43 +0530

    aworkaround code help

--- Kitchen_king.py
+++ Kitchen_king.py
@@ -1,15 +1,40 @@
-from flask import Flask , render_template , redirect , url_for
+from flask import Flask , render_template , redirect , url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:xsATAAJYKKFoTbtiKayOCjSKmiinPQFw@viaduct.proxy.rlwy.net:49669/railway'
+app.config['SECRET_KEY'] = 'ALSKJFDAASDFSADFLSDKFASDF'
+
+db = SQLAlchemy(app)
+
+class form_inputs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=True)
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/')
-
-postgresql://postgres:EKzXCReadsXbcExUVTemkPkUbpLPMaYZ@viaduct.proxy.rlwy.net:10535/railway
-
 def home():
     return render_template('index.html')
-@app.route('/feedback')
-def feedbacksbmit():
-    return redirect(url_for('home'))
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def submit_feedback():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        db.session.add(form_inputs(full_name=name, email=email, message=message)) # entry added into db
+        db.session.commit() # entry commited into db
+
+    data = form_inputs.query.all() # SELECT * from form_inputs
+
+    return render_template('feedback.html', data=data)
 
 app.run(debug=True, host='0.0.0.0', port=8080)
